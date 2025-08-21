40b7229e079c339008729af679d6343af5442901
pragyasharma00108-hash <pragya.sharma.00108@gmail.com>
2025-08-21 12:30:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ useEffect(() => {
   return (
     <div style={styles.container}>
       <div style={styles.chatBox}>
-        <h2 style={styles.title}>🤖 Hey! Sarcasto </h2>
+        <h2 style={styles.title}>🤖 Hi, Sarcasto </h2>
 
         <div style={styles.messagesContainer}>
           {messages.length === 0 ? (
